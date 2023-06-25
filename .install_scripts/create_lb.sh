47ccb005a44bee8720e1194d3553fcae62923841
m4d3bug <m4d3bug@gmail.com>
2023-06-25 20:30:46 +0800

    Update create_lb.sh

--- .install_scripts/create_lb.sh
+++ .install_scripts/create_lb.sh
@@ -66,4 +66,4 @@ echo -n "====> Waiting for SCP result of hosts to LB VM: "
 scp -i sshkey /etc/hosts.${CLUSTER_NAME} root@"$LBIP":/etc || err "SCP to lb.${CLUSTER_NAME}.${BASE_DOM} failed"; ok
 
 echo -n "====> Waiting for restart dnsmasq on LB VM: "
-ssh -i sshkey root@"$LBIP" systemctl restart dnsmasq || err "Restart Dnsmasq on lb.${CLUSTER_NAME}.${BASE_DOM} failed"; ok
+ssh -i sshkey "root@$LBIP" systemctl restart dnsmasq || err "Restart Dnsmasq on lb.${CLUSTER_NAME}.${BASE_DOM} failed"; ok
