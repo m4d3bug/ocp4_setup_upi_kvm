ba17821f8bcf21ce4d32d5ad88849ba834b99b05
m4d3bug <m4d3bug@gmail.com>
2023-12-07 21:37:04 +0800

    Update create_nodes.sh

--- .install_scripts/create_nodes.sh
+++ .install_scripts/create_nodes.sh
@@ -39,6 +39,7 @@ virt-install --name ${CLUSTER_NAME}-master-${i} \
   --os-type linux --os-variant rhel7.0 \
   --network network=${VIR_NET},model=virtio --noreboot --noautoconsole \
   --location rhcos-install/ \
+  --noreboot \
   --extra-args "nomodeset rd.neednet=1 coreos.inst=yes coreos.inst.install_dev=vda nameserver=${LBIP} ${RHCOS_I_ARG}=http://${LBIP}:${WS_PORT}/${IMAGE} coreos.inst.ignition_url=http://${LBIP}:${WS_PORT}/master.ign" > /dev/null || err "Creating master-${i} vm failed "; ok
 done
 
